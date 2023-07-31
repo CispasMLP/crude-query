0e0c82f8de50a23d913945699a8ea707fc1c84f7
Crispas Makanani <crispasmakanani@gmail.com>
2023-07-31 06:27:29 +0200

    delete

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -6,28 +6,38 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { IconButton } from "@mui/material";
+import useDeleteId from '../../hooks/useDeleteId';
 
 
 const Table1=({info:any})=>{
   const { data} = useData();
-  
+  const { deleteInfoById } = useDeleteId();
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell >TITLE</TableCell>
-            <TableCell >DESCRIPTION</TableCell>
-            <TableCell >ACTION</TableCell>
+            <TableCell>TITLE</TableCell>
+            <TableCell>DESCRIPTION</TableCell>
+            <TableCell>ACTION</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data?.map((row) => (
-            <TableRow key={row.id}sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-              <TableCell >{row.title}</TableCell>
-              <TableCell >{row.description}</TableCell>
-              <TableCell >{row.published}</TableCell>
-       
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell>{row.title}</TableCell>
+              <TableCell>{row.description}</TableCell>
+              <TableCell>
+                <IconButton aria-label="delete" onClick={deleteInfoById}>
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
